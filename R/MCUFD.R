5b15d74c40afaf7b68702f5cccb6bfe5300a8c6e
adam <ad866@cam.ac.uk>
2020-05-10 22:28:29 +0200

    Update MCUFD example

--- R/MCUFD.R
+++ R/MCUFD.R
@@ -283,8 +283,7 @@ setMethod("MCUFD_plot",
 #'    exclCod <- c("ATT", "TGT")
 #'    MCUFD_tmp <- MCUFD(virusCF, exclude = exclCod, norm = TRUE)
 #'    enrich_tmp <- MCUFD_enrich(
-#'        MCUFD_tmp, n = 100, rank = "Phylum", plot = TRUE, pthresh = 0.01,
-#'        fname = "enrich_tmp2", height = 5, width = 7
+#'        MCUFD_tmp, n = 100, rank = "Phylum", save = FALSE, pthresh = 0.01
 #'    )
 #'
 #' @export
